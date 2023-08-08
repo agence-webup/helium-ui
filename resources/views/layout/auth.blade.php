64078b59452e5c85ad5bbbdf8edbd43524cd651a
val <valentin@agence-webup.com>
2023-08-08 22:40:45 +0200

    feat(auth): add auth layout

---
+++ resources/views/layout/auth.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login on Helium</title>
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    @vite(['resources/admin/css/tailwind.css'])
+</head>
+
+<body class="flex min-h-screen items-center justify-center bg-slate-200">
+    @yield('content')
+</body>
+
+</html>
